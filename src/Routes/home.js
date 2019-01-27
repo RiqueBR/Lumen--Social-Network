01e242526e2f768bc98abc14d7155fbb1b301c51
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-01-27 18:06:46 +0000

    work on the routes

--- src/Routes/home.js
+++ src/Routes/home.js
@@ -22,18 +22,35 @@ class Home extends Component {
         this.setState({ [e.target.name] : e.target.value});
     };
 
-
+    // Post to Database
+    new = e => {
+        this.setState({loading: true});
+        e.preventDefault();
+        let title = this.state.title;
+        let description = this.state.picture;
+        let picture = this.state.picture;
+        const {currentUser} = fire.auth();
+        fire
+        .database()
+        .ref(`feed/`)
+        .push({title, description, picture, user: currentUser.email})
+        .then(() => {
+            this.setState({loading: false})
+        })
+    }
 
     render() {
         return (
             <div>
                 <input value={this.state.title} onChange={this.handleChange}
                    name="title" className="form-control mb-2" placeholder="title" required/>
-                <input value={this.state.picture} onCHange={this.handleChange} 
+                <input value={this.state.picture} onChange={this.handleChange} 
                    name="picture" className="form-control mb-2" placeholder="Picture URL" required/>
                 <textarea row="9" value={this.state.description} onChange={this.handleChange}
                    name="description" className="form-control mb-2" placeholder="Description" required/>
-
+                <Button color="dark" onClick={this.new} className="mb-5" block>
+                    SUBMIT
+                </Button>
             </div>
         );
     }
